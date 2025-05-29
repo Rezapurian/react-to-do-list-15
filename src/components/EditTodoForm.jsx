35d081baa8840de6a7ec0c51bdb2219c70aafc5b
reza <rezapourian69@gmail.com>
2025-05-29 17:15:53 +0430

    react-15

---
+++ src/components/EditTodoForm.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const EditTodoForm = ({ editTodo, task }) => {
+  const [value, setValue] = useState(task.task);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    editTodo(task.id, value);
+    setValue("");
+  };
+
+  return (
+    <form className="todo-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        className="todo-input"
+        value={value}
+        placeholder="Update Task"
+        onChange={(e) => setValue(e.target.value)}
+      />
+      <button type="submit" className="todo-btn">
+        Update Task
+      </button>
+    </form>
+  );
+};
+
+export default EditTodoForm;
---
+++ src/components/Todo.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaTrash, FaPenToSquare } from "react-icons/fa6";
+
+const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
+  return (
+    <div className="todo">
+      <p
+        onClick={() => toggleComplete(task.id)}
+        className={`inCompleted ${task.completed ? "completed" : ""}`}
+      >
+        {task.task}
+      </p>
+      <div>
+        <FaPenToSquare
+          className="icon edit-icon"
+          onClick={() => editTodo(task.id)}
+        />
+        <FaTrash className="delete-icon" onClick={() => deleteTodo(task.id)} />
+      </div>
+    </div>
+  );
+};
+
+export default Todo;
